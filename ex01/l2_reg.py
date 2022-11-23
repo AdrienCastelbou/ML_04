30695cbb8913c784a39b6aa46895f23f880c4a1d
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-23 11:10:47 +0100

    1 ok

---
+++ ex01/l2_reg.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def iterative_l2(theta):
+    res = 0
+    for i in range(theta.shape[0]):
+        if i > 0:
+            res += theta[i] ** 2
+    return float(res)
+
+def l2(theta):
+    prime_theta = np.array(theta)
+    prime_theta[0][0] = 0
+    return float(prime_theta.T.dot(prime_theta))
+
+
+def main_test():
+    x = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
+    print(iterative_l2(x))
+    print(l2(x))
+    y = np.array([3,0.5,-6]).reshape((-1, 1))
+    print(iterative_l2(y))
+    print(l2(y))
+
+if __name__ == "__main__":
+    main_test()
