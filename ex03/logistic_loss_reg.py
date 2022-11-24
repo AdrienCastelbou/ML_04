db5744f9e88e17a102bbbcec52dab26e3a9666fc
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-24 16:54:33 +0100

    3 ok

--- ex03/logistic_loss_reg.py
+++ ex03/logistic_loss_reg.py
@@ -1,14 +1,21 @@
 import numpy as np
 
 def reg_log_loss_(y, y_hat, theta, lambda_):
-    def loss_(eps=1e-15):
+    try:
+        if type(y) != np.ndarray or type(y_hat) != np.ndarray or type(theta) != np.ndarray or type(lambda_) != float:
+            return None
+        if y.shape[1] != 1 or y.shape != y_hat.shape or theta.shape[1] != 1:
+            return None
+        if len(y) == 0 or len(y_hat) == 0 or len(theta) == 0:
+            return None
         v_ones = np.ones((y.shape[0], 1))
-        return - float(y.T.dot(np.log(y_hat + eps)) + (v_ones - y).T.dot(np.log(v_ones - y_hat + eps))) / y.shape[0]
-    def l2():
+        loss = - float(y.T.dot(np.log(y_hat + 1e-15)) + (v_ones - y).T.dot(np.log(v_ones - y_hat + 1e-15))) / y.shape[0]
         prime_theta = np.array(theta)
         prime_theta[0][0] = 0
-        return float(prime_theta.T.dot(prime_theta))
-    return (loss_() + (lambda_ / (2 * y.shape[0])) * l2())
+        l2 = float(prime_theta.T.dot(prime_theta))
+        return (loss + (lambda_ / (2 * y.shape[0])) * l2)
+    except:
+        return None
 
 
 def main_test():
