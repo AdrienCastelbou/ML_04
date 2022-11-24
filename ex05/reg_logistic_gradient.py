11b311ce940ed7f66eb27fa5b70f041d9e8fa6e2
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-24 17:12:06 +0100

    5 ok

--- ex05/reg_logistic_gradient.py
+++ ex05/reg_logistic_gradient.py
@@ -14,19 +14,41 @@ def logistic_predict_(x, theta):
 
 
 def reg_logistic_grad(y, x, theta, lambda_):
-    reg_nabla = np.zeros(theta.shape)
-    for x_i, y_i, y_hat_i in zip(x, y, logistic_predict_(x, theta)):
-        reg_nabla[0] += y_hat_i - y_i
-        reg_nabla[1:] += (y_hat_i - y_i) * x_i.reshape(-1, 1)
-    reg_nabla[1:] += lambda_ * theta[1:]
-    return reg_nabla / x.shape[0]
+    try:
+        if type(x) != np.ndarray or type(y) != np.ndarray or type(theta) != np.ndarray or not isinstance(lambda_, (int, float)):
+            return None
+        if theta.shape[0] != x.shape[1] + 1:
+            return None
+        if y.shape[1] != 1:
+            return None
+        if not len(x) or not len(y) or not len(theta):
+            return None 
+        reg_nabla = np.zeros(theta.shape)
+        for x_i, y_i, y_hat_i in zip(x, y, logistic_predict_(x, theta)):
+            reg_nabla[0] += y_hat_i - y_i
+            reg_nabla[1:] += (y_hat_i - y_i) * x_i.reshape(-1, 1)
+        reg_nabla[1:] += lambda_ * theta[1:]
+        return reg_nabla / x.shape[0]
+    except:
+        return None
 
 def vec_reg_logistic_grad(y, x, theta, lambda_):
-    x = np.hstack((np.ones((x.shape[0], 1)), x))
-    prime_theta = np.array(theta)
-    prime_theta[0][0] = 0
-    nabla_J = (x.T.dot(1 / (1 + np.e ** - x.dot(theta)) - y) + lambda_ * prime_theta)/ x.shape[0]
-    return nabla_J 
+    try:
+        if type(x) != np.ndarray or type(y) != np.ndarray or type(theta) != np.ndarray or not isinstance(lambda_, (int, float)):
+            return None
+        if theta.shape[0] != x.shape[1] + 1:
+            return None
+        if y.shape[1] != 1:
+            return None
+        if not len(x) or not len(y) or not len(theta):
+            return None 
+        x = np.hstack((np.ones((x.shape[0], 1)), x))
+        prime_theta = np.array(theta)
+        prime_theta[0][0] = 0
+        nabla_J = (x.T.dot(1 / (1 + np.e ** - x.dot(theta)) - y) + lambda_ * prime_theta)/ x.shape[0]
+        return nabla_J 
+    except:
+        return None
 
 def main_test():
     x = np.array([[0, 2, 3, 4], [2, 4, 5, 5], [1, 3, 2, 7]])
