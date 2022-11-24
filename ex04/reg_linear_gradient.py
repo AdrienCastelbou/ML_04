a7cbc2dd7ec72d7f2794ea9a2ad66b7b2569a991
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-24 17:03:06 +0100

    4 ok

--- ex04/reg_linear_gradient.py
+++ ex04/reg_linear_gradient.py
@@ -1,28 +1,53 @@
 import numpy as np
 
 def predict_(x, theta):
-    if type(x) != np.ndarray or type(theta) != np.ndarray:
+    try:
+        if type(x) != np.ndarray or type(theta) != np.ndarray:
+            return None
+        if not len(x) or not len(theta):
+            return None
+        extended_x = np.hstack((np.ones((x.shape[0], 1)), x))
+        return extended_x.dot(theta)
+    except:
         return None
-    if not len(x) or not len(theta):
-        return None
-    extended_x = np.hstack((np.ones((x.shape[0], 1)), x))
-    return extended_x.dot(theta)
 
 def reg_linear_grad(y, x, theta, lambda_):
-    nabla_J = np.zeros(theta.shape)
-    for x_i, y_i, y_hat_i in zip(x, y, predict_(x, theta)):
-        nabla_J[0] += (y_hat_i - y_i)
-        nabla_J[1:] += (y_hat_i - y_i) * x_i.reshape(-1, 1)
-    nabla_J[1: ] += lambda_ * theta[1:]
-    return nabla_J / y.shape[0]
+    try:
+        if type(x) != np.ndarray or type(y) != np.ndarray or type(theta) != np.ndarray or not isinstance(lambda_, (int, float)):
+            return None
+        if theta.shape[0] != x.shape[1] + 1:
+            return None
+        if y.shape[1] != 1:
+            return None
+        if not len(x) or not len(y) or not len(theta):
+            return None
+        nabla_J = np.zeros(theta.shape)
+        for x_i, y_i, y_hat_i in zip(x, y, predict_(x, theta)):
+            nabla_J[0] += (y_hat_i - y_i)
+            nabla_J[1:] += (y_hat_i - y_i) * x_i.reshape(-1, 1)
+        nabla_J[1: ] += lambda_ * theta[1:]
+        return nabla_J / y.shape[0]
+    except:
+        return None
 
 
 def vec_reg_linear_grad(y, x, theta, lambda_):
-    x = np.hstack((np.ones((x.shape[0], 1)), x))
-    prime_theta = np.array(theta)
-    prime_theta[0][0] = 0
-    nabla_J = (x.T.dot(x.dot(theta) - y) + lambda_ * prime_theta)/ x.shape[0]
-    return nabla_J 
+    try:
+        if type(x) != np.ndarray or type(y) != np.ndarray or type(theta) != np.ndarray or not isinstance(lambda_, (int, float)):
+            return None
+        if theta.shape[0] != x.shape[1] + 1:
+            return None
+        if y.shape[1] != 1:
+            return None
+        if not len(x) or not len(y) or not len(theta):
+            return None 
+        x = np.hstack((np.ones((x.shape[0], 1)), x))
+        prime_theta = np.array(theta)
+        prime_theta[0][0] = 0
+        nabla_J = (x.T.dot(x.dot(theta) - y) + lambda_ * prime_theta)/ x.shape[0]
+        return nabla_J
+    except:
+        return None
 
 def main_test():
     x = np.array([[ -6, -7, -9], [ 13, -2, 14], [ -7, 14, -1], [-8, -4, 6], [-5, -9, 6], [ 1, -5, 11], [9,-11, 8]])
