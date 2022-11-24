31b4d09f15f36fb98f6e0a8972bd43e556214a39
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-24 16:26:32 +0100

    00 ok

--- ex00/polynomial_model_extended.py
+++ ex00/polynomial_model_extended.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 def add_polynomial_features(x, power):
-    poly_x = np.array(x)
-    for i in range(2, power + 1):
-        for col in x.T:
-            poly_x = np.hstack((poly_x, (col ** i).reshape((-1, 1))))
-    return poly_x
+    try:
+        if type(x) != np.ndarray or not isinstance(power, (int, float)):
+            return None
+        poly_x = np.array(x)
+        for i in range(2, power + 1):
+            for col in x.T:
+                poly_x = np.hstack((poly_x, (col ** i).reshape((-1, 1))))
+        return poly_x
+    except:
+        return None
 
 
 def main_test():
