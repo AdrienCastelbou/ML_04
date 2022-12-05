8b9b03312d13f58b950b0fcafc417c8d06a89adc
Adrien Castelbou <acastelbou1@gmail.com>
2022-12-05 21:57:07 +0100

    croos gene

--- ex07/benchmark_train.py
+++ ex07/benchmark_train.py
@@ -9,25 +9,38 @@ import matplotlib.pyplot as plt
 
 
 def save_models(results):
-    file = open('zscore-models.pickle', 'wb')
+    file = open('cross-models.pickle', 'wb')
     pickle.dump(results, file)
     file.close()
 
+
+def evaluate_model(myR, x, y):
+    y_hat = myR.predict_(x)
+    mse = MyRidge.mse_(y, y_hat)
+    print(mse)
+    return mse
+
 def perform_regression(x, y, lambda_):
-    print("start")
-    theta = np.random.rand(x.shape[1] + 1, 1).reshape(-1, 1)
-    myR =  MyRidge(theta, alpha = 1e-2, max_iter = 1000000, lambda_=lambda_)
-    myR.fit_(x, y)
-    print("end")
-    return myR
+    x_cross, y_cross = data_cross_splitter(x, y, 4)
+    mses = []
+    for k in range(len(x_cross)):
+        print(f"fold {k}")
+        x_folds_test = np.concatenate([x_fold for i, x_fold in enumerate(x_cross) if i!=k])
+        x_fold_evaluate = x_cross[k]
+        y_folds_test = np.concatenate([y_fold for i, y_fold in enumerate(y_cross) if i!=k])
+        y_fold_evaluate = y_cross[k]
+        theta = np.random.rand(x_folds_test.shape[1] + 1, 1).reshape(-1, 1)
+        myR =  MyRidge(theta, alpha = 1e-2, max_iter = 100000, lambda_=lambda_)
+        myR.fit_(x_folds_test, y_folds_test)
+        mses.append(evaluate_model(myR, x_fold_evaluate, y_fold_evaluate))
+    return myR, np.mean(mses)
 
 
 def regression_engine(x, y):
-    x_train, x_test, y_train, y_test = data_spliter(x, y, 0.8)
-    x_cross, y_cross = data_cross_splitter(x, y)
-    x_train = add_polynomial_features(x_train, 4)
+    x_train = add_polynomial_features(x, 4)
     x_train = normalize(x_train)
     models = {}
+    models_score = {}
     for w_rank in range(1, 5):
         w_features = [0, 3, 6, 9][:w_rank]
         for d_rank in range(1, 5):
@@ -36,9 +49,10 @@ def regression_engine(x, y):
                 t_features = [2, 5, 8, 11][:t_rank]
                 x_train_features = x_train[:, np.concatenate((w_features, d_features, t_features))]
                 for l, lambda_ in enumerate([0., 0.2, 0.4, 0.6, 0.8]):
-                    print(f"w{w_rank}d{d_rank}t{t_rank}λ{lambda_}")
-                    current = perform_regression(x_train_features, y_train, lambda_)
-                    models[f"w{w_rank}d{d_rank}t{t_rank}λ{lambda_}"] = current#perform_regression(x_train_features, y_train, lambda_)
+                    classifiers_rank = f"w{w_rank}d{d_rank}t{t_rank}λ{lambda_}"
+                    print(classifiers_rank)
+                    models[classifiers_rank], models_score[classifiers_rank] = perform_regression(x_train_features, y, lambda_)
+                    print("classifiers_rank average mse : ", models_score[classifiers_rank] )
     save_models(models)
 
 
